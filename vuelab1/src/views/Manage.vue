67032b0777ffca0b72cb0ce17b7b7f10ae709090
NROLP <mikoabaja54@gmail.com>
2023-10-09 23:05:42 +0800

    main
    setting up from documentation

---
+++ vuelab1/src/views/Manage.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="manage">
+        <h1>Manage Page</h1>
+        <p>This is the manage page content.</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Manage'
+}
+</script>
+
+<style scoped>
+/* Add your custom CSS styles here */
+</style>
